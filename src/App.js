2a7d106b48fe5427aa6f4c5cfa0fca7cf7bb6682
shinhee <tlsgml656@gmail.com>
2023-01-11 18:34:02 +0900

    Navbar 작업

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        pasdf
-      </header>
+      <NavBar></NavBar>
     </div>
   );
 }
