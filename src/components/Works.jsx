233b33c08c04f2f6df691733cecd737abe64b354
shinhee <tlsgml656@gmail.com>
2023-01-13 19:31:05 +0900

    works 페이지 작업

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import './Works.scss'
-import { Link } from 'react-router-dom' 
+import './Works.scss' 
 
 const Works = () => {
   return (
     <div className='works'>
       <div className="works-container">
+        <div className="work">
+          <h3>site1</h3>
+          <span className='bar'></span>
+          <p>오투요소수</p>
+          <a href='http://www.o2urea.com/' className='btn' target='_blank' rel='noopener noreferrer'></a>
+        </div>
+        <div className="work">
+          <h3>site1</h3>
+          <span className='bar'></span>
+          <p>오투요소수</p>
+          <a href='http://www.o2urea.com/' className='btn' target='_blank' rel='noopener noreferrer'></a>
+        </div>
         <div className="work">
           <h3>site1</h3>
           <span className='bar'></span>
